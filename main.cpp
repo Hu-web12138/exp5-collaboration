ae3625e027a4e21cf728b0745f0bfa1bc2183af7
Hu Yilin <3023902498@qq.com>
2025-11-28 20:24:28 +0800

    Update by Owner

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
 	cout<<"Hello!I am HuYilin!\nI major in CS\n";
-	// 2023150043 胡依麟
+	// 2023150043是胡依麟同学的学号
 	for(int i = 5; i > 0; i--){
 		cout << i << " ";
 	}
