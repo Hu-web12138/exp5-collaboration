5196b9454fdf37bc889766df66df069475284d14
Hu Yilin <3023902498@qq.com>
2025-11-28 18:49:17 +0800

    Add a for loop

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout<<"Hello!I am HuYilin!\nI major in CS\n";
+	// 2023150043 胡依麟
+	for(int i = 5; i > 0; i--){
+		cout << i << " ";
+	}
+	cout << endl;
+	return 0;
+}
